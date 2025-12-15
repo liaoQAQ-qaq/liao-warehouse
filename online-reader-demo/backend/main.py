8301727ee85d34bd070a611a47d405fafe2a9601
liaoQAQ-qaq <liaozhenhao@zhuquezhihui.com>
2025-12-15 03:34:41 +0000

    修改通用化文件

--- online-reader-demo/backend/main.py
+++ online-reader-demo/backend/main.py
@@ -3,7 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from pydantic import BaseModel  # 新增导入
 from typing import List  # 新增导入
-from models import Base, engine, SessionLocal, Novel
+from backend.models import Base, engine, SessionLocal, Novel
+
 
 # 初始化数据库
 Base.metadata.create_all(bind=engine)
